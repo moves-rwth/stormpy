767d32fb979329259251571d93e2c7d37c3859fb
Lutz Klinkenberg <lutz.klinkenberg@rwth-aachen.de>
2017-03-01 14:27:30 +0100

    Edited the init test case to test if the variable name is set the right way.

--- tests/core/test_variable.py
+++ tests/core/test_variable.py
@@ -3,9 +3,10 @@ import pycarl
 class TestCore:
     def test_init(self):
         var = pycarl.Variable("x")
+        assert var.name == "x"	
 
     def test_product(self):
         x = pycarl.Variable("x")
         y = pycarl.Variable("y")
-        product = x * y 
+        product = x * y
     
