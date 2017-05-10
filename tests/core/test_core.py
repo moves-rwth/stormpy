2a7db171f68c2c3df22af31db3ce9c9c20f62bce
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-05-10 21:59:00 +0200

    Test whether pickling support is realised for  (data) classes

--- tests/core/test_core.py
+++ tests/core/test_core.py
@@ -1,3 +1,7 @@
+import inspect
+import enum
+import builtins
+
 
 class TestCore:
     def test_init(self):
@@ -6,4 +10,17 @@ class TestCore:
     def test_import(self):
         import pycarl
         import pycarl.formula
+
+    def test_pickle(self):
+        import pycarl
+        import pycarl.gmp
+        import pycarl.cln
+        import pycarl.formula
+
+        for mod in [pycarl, pycarl.formula, pycarl.gmp, pycarl.cln]:
+            for name, obj in inspect.getmembers(mod):
+                if inspect.isclass(obj):
+                    assert  issubclass(obj, enum.Enum) or issubclass(obj, builtins.Exception) or (hasattr(obj, '__getstate__') and inspect.isroutine(getattr(obj, '__getstate__')))
+                    assert  issubclass(obj, enum.Enum) or issubclass(obj, builtins.Exception) or (hasattr(obj, '__setstate__') and inspect.isroutine(getattr(obj, '__setstate__')))
+
     
