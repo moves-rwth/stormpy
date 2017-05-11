b80d4bfe34f23e305cd242af1080bbcd4a93cee6
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-05-11 17:07:59 +0200

    Tests for variable pickle and equality

--- tests/core/test_variable.py
+++ tests/core/test_variable.py
@@ -1,12 +1,33 @@
 import pycarl
+import pickle
 
-class TestCore:
+class TestVariable:
     def test_init(self):
         var = pycarl.Variable("x")
-        assert var.name == "x"	
+        assert var.name == "x"
+        assert not var.is_no_variable
+
+    def test_eq(self):
+        var1 = pycarl.Variable("x")
+        var2 = pycarl.Variable("x")
+        assert var1 == var2
+        var3 = pycarl.Variable("y")
+        assert var1 != var3
 
     def test_product(self):
         x = pycarl.Variable("x")
         y = pycarl.Variable("y")
         product = x * y
+
+    def test_pickle(self):
+        var1 = pycarl.Variable("x")
+        ser = pickle.dumps(var1)
+        var2 = pickle.loads(ser)
+        pycarl.Variable("x")
+        assert var1 == var2
+        var3 = pycarl.Variable("y")
+        ser = pickle.dumps(var3)
+        var4 = pickle.loads(ser)
+        assert var3 == var4
+        assert var4 != var2
     
