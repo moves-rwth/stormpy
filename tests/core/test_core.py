2183a59ec802029ec69f7fc9f6b5318b40a89f63
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-06-01 11:26:24 +0200

    Import modules in test_core only when needed

--- tests/core/test_core.py
+++ tests/core/test_core.py
@@ -1,7 +1,3 @@
-import inspect
-import enum
-import builtins
-
 
 class TestCore:
     def test_init(self):
@@ -17,10 +13,13 @@ class TestCore:
         import pycarl.cln
         import pycarl.formula
 
+        import inspect
+        import enum
+        import builtins
+
         for mod in [pycarl, pycarl.formula, pycarl.gmp, pycarl.cln]:
             for name, obj in inspect.getmembers(mod):
                 if inspect.isclass(obj):
                     assert  issubclass(obj, enum.Enum) or issubclass(obj, builtins.Exception) or (hasattr(obj, '__getstate__') and inspect.isroutine(getattr(obj, '__getstate__')))
                     assert  issubclass(obj, enum.Enum) or issubclass(obj, builtins.Exception) or (hasattr(obj, '__setstate__') and inspect.isroutine(getattr(obj, '__setstate__')))
 
-    
