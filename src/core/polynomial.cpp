968fde866b14ea82f065d7f12bf313350e7b614a
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-06-16 13:30:48 +0200

    operator between variable and polynomial

--- src/core/polynomial.cpp
+++ src/core/polynomial.cpp
@@ -35,6 +35,7 @@ void define_polynomial(py::module& m) {
         .def(py::self - carl::Variable())
         .def("__sub__", [](const Polynomial& lhs, Monomial::Arg rhs) -> Polynomial { return lhs - rhs; })
         .def("__rsub__", [](const Polynomial& lhs, Monomial::Arg rhs) -> Polynomial { return rhs - lhs; })
+        .def("__rsub__", [](const Polynomial& lhs, carl::Variable::Arg rhs) -> Polynomial { return rhs - lhs; })
         .def(py::self - Term())
         .def(py::self - py::self)
 
