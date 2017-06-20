593d6239a5134bc786b65c95d8c3885e921d8b05
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-06-20 12:36:14 +0200

    Cleaned up monomial code

--- src/core/monomial.cpp
+++ src/core/monomial.cpp
@@ -13,26 +13,13 @@
 
 void define_monomial(py::module& m) {
     py::class_<Monomial, std::shared_ptr<Monomial>>(m, "Monomial")
-//        .def("__init__", [] (Monomial& m, const carl::Variable& v, carl::exponent e){
-//            m = carl::createMonomial(v, e);
-//        })
-
-            // From monomial, todo clean
-            //.def(PY_DIV, [](const Monomial::Arg& lhs, const RationalFunction& rhs) { return RationalFunction(Polynomial(lhs)) / rhs; })
-            //.def(PY_DIV, [](const Monomial::Arg& lhs, const Polynomial& rhs) { return RationalFunction(Polynomial(lhs)) / rhs; })
-            //.def(PY_DIV, [](const Monomial::Arg& lhs, const Term& rhs) { return RationalFunction(Polynomial(lhs)) / rhs; })
-            //.def(PY_DIV, [](const Monomial::Arg& lhs, const Monomial::Arg& rhs) { return RationalFunction(Polynomial(lhs)) / rhs; })
-            //.def(PY_DIV, [](const Monomial::Arg& lhs, const carl::Variable& rhs) { return RationalFunction(Polynomial(lhs)) / rhs; })
-            //.def(PY_DIV, [](const Monomial::Arg& lhs, Rational rhs) { return lhs / rhs; })
 
         .def("__pow__", [](const Monomial::Arg& var, carl::uint exp) {return var->pow(exp);})
-            .def("__mul__",  static_cast<Monomial::Arg (*)(const Monomial::Arg&, const Monomial::Arg&)>(&carl::operator*))
-            .def("__mul__",  static_cast<Monomial::Arg (*)(const Monomial::Arg&, carl::Variable::Arg)>(&carl::operator*))
-            .def("__mul__",  static_cast<Monomial::Arg (*)(carl::Variable::Arg, const Monomial::Arg&)>(&carl::operator*))
+        .def("__mul__",  static_cast<Monomial::Arg (*)(const Monomial::Arg&, const Monomial::Arg&)>(&carl::operator*))
+        .def("__mul__",  static_cast<Monomial::Arg (*)(const Monomial::Arg&, carl::Variable::Arg)>(&carl::operator*))
+        .def("__mul__",  static_cast<Monomial::Arg (*)(carl::Variable::Arg, const Monomial::Arg&)>(&carl::operator*))
 
-
-
-            .def("__pos__", [](const Monomial::Arg& var) {return Monomial::Arg(var);})
+        .def("__pos__", [](const Monomial::Arg& var) {return Monomial::Arg(var);})
 
         .def_property_readonly("tdeg", [](const Monomial::Arg& arg) { return arg->tdeg();} )
         .def_property_readonly("exponents", [](const Monomial::Arg& arg) { return arg->exponents();} )
@@ -47,9 +34,9 @@ void define_monomial(py::module& m) {
 
     m.def("create_monomial", [] (const carl::Variable& v, carl::exponent e){
         return carl::createMonomial(v, e);
-    });
+    }, "Create monomial", py::arg("variable"), py::arg("exponent"));
     m.def("clear_monomial_pool", [](){
-	carl::MonomialPool::getInstance().clear();
+        carl::MonomialPool::getInstance().clear();
     }, "Clear monomial pool and remove all monomials");
 
 }
