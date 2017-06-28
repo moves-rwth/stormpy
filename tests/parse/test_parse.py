9b0ab9755af89866e54ce553899c66f4b869af9a
Lutz Klinkenberg <lutz.klinkenberg@rwth-aachen.de>
2017-06-28 19:00:01 +0200

    Test for parsing

---
+++ tests/parse/test_parse.py
@@ -0,0 +1,13 @@
+import pycarl
+import pycarl.parse
+import pycarl.cln.parse, pycarl.gmp.parse
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from package_selector import PackageSelector
+
+
+class TestParse(PackageSelector):
+    def test_parse_number(self, package):
+        num = package.parse.parse("2")
+        assert str(num) == "2"
