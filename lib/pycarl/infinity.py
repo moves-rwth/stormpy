b967ec6852ac73e80829bac296c4eadde501564a
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-08-04 17:08:28 +0200

    arithmetic operations with infinity, added missing import sys for eq checking

--- lib/pycarl/infinity.py
+++ lib/pycarl/infinity.py
@@ -1,7 +1,10 @@
-
 import math
+import sys
 
 class Infinity:
+    """
+    Class representing infinity and minus infinity
+    """
     def __init__(self, negated = False):
         self.negated = negated
 
@@ -24,13 +27,41 @@ class Infinity:
         if isinstance(other, Infinity) and not other.negated: return False
         return not self.negated
 
-
     def __eq__(self, other):
         if sys.version_info[1] >= 5:
             return (isinstance(other, Infinity) and other.negated == self.negated) or (not self.negated and other == math.inf)
         else:
             return (isinstance(other, Infinity) and other.negated == self.negated)
 
+    def __add__(self, other):
+        return self
+
+    def __radd__(self, other):
+        return self
+
+    def __sub__(self, other):
+        return self
+
+    def __rsub__(self, other):
+        return self
+
+    def __mul__(self, other):
+        if other < 0:
+            return -self
+        return self
+
+    def __rmul__(self, other):
+        if other < 0:
+            return -self
+        return self
+
+    def __div__(self, other):
+        if other < 0:
+            return - self
+        return self
+
+    def __rdiv__(self, other):
+        return 0
 
     def __str__(self):
         return "-infty" if self.negated else "infty"
