725611baf657a04d46d7ff5350fd3223e5cd6fef
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-09-01 14:49:50 +0200

    Fix after removal of relationToString in carl

--- src/formula/relation.cpp
+++ src/formula/relation.cpp
@@ -1,14 +1,16 @@
 #include "relation.h"
+#include "src/helpers.h"
+
 #include <carl/formula/Formula.h>
 
 void define_relation(py::module& m) {
     py::enum_<carl::Relation>(m, "Relation")
-            .value("EQ", carl::Relation::EQ)
-            .value("NEQ", carl::Relation::NEQ)
-            .value("LESS", carl::Relation::LESS)
-            .value("LEQ", carl::Relation::LEQ)
-            .value("GREATER", carl::Relation::GREATER)
-            .value("GEQ", carl::Relation::GEQ)
-            .def("__str__", [](carl::Relation r) { return carl::relationToString(r); })
-            ;
+        .value("EQ", carl::Relation::EQ)
+        .value("NEQ", carl::Relation::NEQ)
+        .value("LESS", carl::Relation::LESS)
+        .value("LEQ", carl::Relation::LEQ)
+        .value("GREATER", carl::Relation::GREATER)
+        .value("GEQ", carl::Relation::GEQ)
+        .def("__str__", &streamToString<carl::Relation>)
+    ;
 }
