ce43b9b59d504a7c78bf900e011141e7e0afdcc4
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-10-06 16:29:41 +0200

    Added missing patch version if not given

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ def parse_carl_version(version_string):
     :return: Triple (major, minor, patch)
     """
     elems = version_string.split(".")
+    if len(elems) == 2:
+        elems.append(0)
     if len(elems) != 3:
         sys.exit('Carl version string is ill-formed: "{}"'.format(version_string))
     patch = elems[2].split('-')[0]
