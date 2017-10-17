aacb202fb32a9051275dc6fd4a15b430fbdc87f4
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-10-17 11:14:16 +0200

    Updated dft bindings after Storm-dft refactoring

--- src/dft/dft.cpp
+++ src/dft/dft.cpp
@@ -1,8 +1,8 @@
 #include "dft.h"
 #include "storm-dft/parser/DFTJsonParser.h"
-#include "storm-dft/builder/ExplicitDFTModelBuilderApprox.h"
+#include "storm-dft/builder/ExplicitDFTModelBuilder.h"
 #include "storm/settings/SettingsManager.h"
-#include "storm-dft/settings/modules/DFTSettings.h"
+#include "storm-dft/settings/modules/FaultTreeSettings.h"
 #include "storm-dft/storage/dft/DFTIsomorphism.h"
 
 // Thin wrapper for model building using one formula as argument
@@ -18,15 +18,15 @@ std::shared_ptr<storm::models::sparse::Model<ValueType>> buildModelFromJsonDft(s
         auto colouring = dft.colourDFT();
         symmetries = dft.findSymmetries(colouring);
     }
-    storm::builder::ExplicitDFTModelBuilderApprox<ValueType> builder(dft, symmetries, true);
-    typename storm::builder::ExplicitDFTModelBuilderApprox<ValueType>::LabelOptions labeloptions({}, true);
+    storm::builder::ExplicitDFTModelBuilder<ValueType> builder(dft, symmetries, true);
+    typename storm::builder::ExplicitDFTModelBuilder<ValueType>::LabelOptions labeloptions({}, true);
     builder.buildModel(labeloptions, 0, 0.0);
     return builder.getModel();
 }
 
 void define_dft(py::module& m) {
     m.def("_set_up", []() {
-            storm::settings::addModule<storm::settings::modules::DFTSettings>();
+            storm::settings::addModule<storm::settings::modules::FaultTreeSettings>();
         }, "Initialize Storm-dft");
     // Build model
     m.def("build_sparse_model_from_json_dft", &buildModelFromJsonDft<double>, "Build the model", py::arg("jsonDft"), py::arg("symred") = false);
