d581a95c6540e03a6322837305a345273e2f73ac
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-11-24 18:23:47 +0100

    examples and docu for building models

---
+++ examples/building_models/01-building-models.py
@@ -0,0 +1,29 @@
+import stormpy
+import stormpy.core
+
+import stormpy.examples
+import stormpy.examples.files
+
+
+def example_building_models():
+    path = stormpy.examples.files.drn_ctmc_dft
+    model = stormpy.build_model_from_drn(path)
+    print(model.model_type)
+    print("Number of states: {}".format(model.nr_states))
+
+    # And the parametric
+    path = stormpy.examples.files.drn_pdtmc_die
+    model = stormpy.build_parametric_model_from_drn(path)
+    print(model.model_type)
+    print("Number of states: {}".format(model.nr_states))
+
+    path = stormpy.examples.files.jani_dtmc_die
+    jani_program, properties = stormpy.parse_jani_model(path)
+    model = stormpy.build_model(jani_program)
+    print(model.model_type)
+    print("Number of states: {}".format(model.nr_states))
+
+
+
+if __name__ == '__main__':
+    example_building_models()
--- lib/stormpy/examples/files.py
+++ lib/stormpy/examples/files.py
@@ -19,3 +19,9 @@ prism_pdtmc_die = _path("pdtmc", "parametric_die.pm")
 """Knuth Yao Die -- 2 unfair coins Example"""
 prism_dtmc_brp = _path("dtmc", "brp-16-2.pm")
 """Bounded Retransmission Protocol"""
+drn_ctmc_dft = _path("ctmc", "dft.drn")
+"""DRN format for a CTMC from a DFT"""
+drn_pdtmc_die = _path("pdtmc", "die.drn")
+"""DRN format for a pDTMC for the KY-Die"""
+jani_dtmc_die = _path("dtmc", "die.jani")
+"""Jani Version of Knuth Yao Die Example"""
