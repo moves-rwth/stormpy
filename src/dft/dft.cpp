a7cc7b3086974585415a46fabfd7576f82ecf5ac
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2018-08-06 13:59:11 +0200

    Extended bindings for DFT class

--- src/dft/dft.cpp
+++ src/dft/dft.cpp
@@ -17,11 +17,19 @@ void define_dft(py::module& m) {
 
     // DFT class
     py::class_<DFT<double>, std::shared_ptr<DFT<double>>>(m, "DFT", "DFT")
-        .def("nr_elements", &DFT<double>::nrElements)
+        .def("nr_elements", &DFT<double>::nrElements, "Total number of elements")
+        .def("nr_be", &DFT<double>::nrBasicElements, "Number of basic elements")
+        .def("nr_dynamic", &DFT<double>::nrDynamicElements, "Number of dynamic elements")
+        .def("can_have_nondeterminism", &DFT<double>::canHaveNondeterminism, "Whether the model can contain non-deterministic choices")
+        .def("__str__", &DFT<double>::getInfoString)
     ;
 
     py::class_<DFT<storm::RationalFunction>, std::shared_ptr<DFT<storm::RationalFunction>>>(m, "ParametricDFT", "Parametric DFT")
-        .def("nr_elements", &DFT<storm::RationalFunction>::nrElements)
+        .def("nr_elements", &DFT<storm::RationalFunction>::nrElements, "Total number of elements")
+        .def("nr_be", &DFT<storm::RationalFunction>::nrBasicElements, "Number of basic elements")
+        .def("nr_dynamic", &DFT<storm::RationalFunction>::nrDynamicElements, "Number of dynamic elements")
+        .def("can_have_nondeterminism", &DFT<storm::RationalFunction>::canHaveNondeterminism, "Whether the model can contain non-deterministic choices")
+        .def("__str__", &DFT<storm::RationalFunction>::getInfoString)
     ;
 
 }
