5d41f02e161d066d8a353c28a406d9267f8abd54
Sebastian Junges <sebastian.junges@gmail.com>
2020-04-14 09:41:55 -0700

    state valuations.get_state() by reference, and simple valuations __str__ now only has optional arguments

--- src/storage/valuation.cpp
+++ src/storage/valuation.cpp
@@ -9,7 +9,7 @@
 void define_statevaluation(py::module& m) {
 
     py::class_<storm::storage::sparse::StateValuations, std::shared_ptr<storm::storage::sparse::StateValuations>> statevaluation(m,"StateValuation", "Valuations for explicit states");
-    statevaluation.def("get_state", &storm::storage::sparse::StateValuations::getStateValuation)
+    statevaluation.def("get_state", &storm::storage::sparse::StateValuations::getStateValuation, py::return_value_policy::reference)
             ;
 
 }
@@ -18,5 +18,5 @@ void define_simplevaluation(py::module& m) {
     py::class_<storm::expressions::SimpleValuation, std::shared_ptr<storm::expressions::SimpleValuation>> simplevaluation(m, "SimpleValuation",  "Valuations for storm variables");
     simplevaluation.def("get_boolean_value", &storm::expressions::SimpleValuation::getBooleanValue);
     simplevaluation.def("get_integer_value", &storm::expressions::SimpleValuation::getIntegerValue);
-    simplevaluation.def("__str__", &storm::expressions::SimpleValuation::toString);
+    simplevaluation.def("__str__", &storm::expressions::SimpleValuation::toString, py::arg("pretty")=true);
 }
