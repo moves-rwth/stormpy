339781db4a752715c57f648928e325bc464c9466
hannah <hannah.mertens@rwth-aachen.de>
2020-04-21 22:07:29 +0200

    created gspn example

--- lib/stormpy/examples/files.py
+++ lib/stormpy/examples/files.py
@@ -41,3 +41,5 @@ dft_galileo_hecs = _path("dft", "hecs.dft")
 """DFT example for HECS (Galileo format)"""
 dft_json_and = _path("dft", "and.json")
 """DFT example for AND gate (JSON format)"""
+gspn_pnpro_simple = _path("gspn", "gspn_simple.pnpro")
+"""GSPN example (PNPRO format)"""
