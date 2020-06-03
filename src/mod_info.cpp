0cb3f0cee85252276cdcdef319fb963dc445d55a
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2020-06-03 11:33:15 +0200

    Adaptions due to recently introduced storm-version-info library

--- src/mod_info.cpp
+++ src/mod_info.cpp
@@ -1,5 +1,5 @@
 #include "common.h"
-#include "storm/utility/storm-version.h"
+#include "storm-version-info/storm-version.h"
 
 PYBIND11_MODULE(info, m) {
     m.doc() = "Storm information";
@@ -9,15 +9,15 @@ PYBIND11_MODULE(info, m) {
     options.disable_function_signatures();
 #endif
 
-    py::class_<storm::utility::StormVersion>(m, "Version", "Version information for Storm")
+    py::class_<storm::StormVersion>(m, "Version", "Version information for Storm")
         // static properties are still called with self as argument (which we ignore), see
         // https://pybind11.readthedocs.io/en/master/advanced/classes.html#static-properties
-        .def_property_readonly_static("major", [](py::object /* self */){ return storm::utility::StormVersion::versionMajor; }, "Storm major version.")
-        .def_property_readonly_static("minor", [](py::object /* self */){ return storm::utility::StormVersion::versionMinor; }, "Storm minor version.")
-        .def_property_readonly_static("patch", [](py::object /* self */){ return storm::utility::StormVersion::versionPatch; }, "Storm patch version.")
-        .def_property_readonly_static("development", [](py::object /* self */){ return storm::utility::StormVersion::versionDev; }, "Flag if Storm is development version.")
-        .def_property_readonly_static("short", [](py::object /* self */){ return storm::utility::StormVersion::shortVersionString(); }, "Storm version in short representation.")
-        .def_property_readonly_static("long", [](py::object /* self */){ return storm::utility::StormVersion::longVersionString(); }, "Storm version in long representation.")
-        .def_property_readonly_static("build_info", [](py::object /* self */){ return storm::utility::StormVersion::buildInfo(); }, "Build info for Storm.")
+        .def_property_readonly_static("major", [](py::object /* self */){ return storm::StormVersion::versionMajor; }, "Storm major version.")
+        .def_property_readonly_static("minor", [](py::object /* self */){ return storm::StormVersion::versionMinor; }, "Storm minor version.")
+        .def_property_readonly_static("patch", [](py::object /* self */){ return storm::StormVersion::versionPatch; }, "Storm patch version.")
+        .def_property_readonly_static("development", [](py::object /* self */){ return storm::StormVersion::versionDev; }, "Flag if Storm is development version.")
+        .def_property_readonly_static("short", [](py::object /* self */){ return storm::StormVersion::shortVersionString(); }, "Storm version in short representation.")
+        .def_property_readonly_static("long", [](py::object /* self */){ return storm::StormVersion::longVersionString(); }, "Storm version in long representation.")
+        .def_property_readonly_static("build_info", [](py::object /* self */){ return storm::StormVersion::buildInfo(); }, "Build info for Storm.")
     ;
 }
