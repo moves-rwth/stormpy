f5668000b94b492ade79b78ad795a493455b51b5
hannah <hannah.mertens@rwth-aachen.de>
2020-08-25 13:46:53 +0200

    fixed engines notebook and binder badge

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -248,7 +248,7 @@ nbsphinx_prolog = """
 .. raw:: html
 
     <div class="admonition note">
-      Interactive online version:
+      Interactive version:
       <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/m-hannah/stormpy/master?filepath={{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
     </div>
 """
