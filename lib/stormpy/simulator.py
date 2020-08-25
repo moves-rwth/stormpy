e3467a4bb93abec0dcacfce46cc430b8d73c05ec
Sebastian Junges <sebastian.junges@gmail.com>
2020-08-24 21:47:30 -0700

    simulator does not crash on dtmcs

--- lib/stormpy/simulator.py
+++ lib/stormpy/simulator.py
@@ -120,6 +120,8 @@ class SparseSimulator(Simulator):
             return av_actions
 
     def nr_available_actions(self):
+        if not self._model.is_nondeterministic_model:
+            return 1
         return self._model.get_nr_available_actions(self._engine.get_current_state())
 
     def _report_state(self):
