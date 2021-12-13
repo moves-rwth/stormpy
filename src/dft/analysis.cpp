2449c6a82e085ba167b32e62ed5a08b8bb16dec7
Matthias Volk <m.volk@utwente.nl>
2021-12-13 13:39:13 +0100

    Preliminary support for parametric DFTs

--- src/dft/analysis.cpp
+++ src/dft/analysis.cpp
@@ -3,8 +3,6 @@
 #include "storm-dft/parser/DFTJsonParser.h"
 #include "storm-dft/builder/ExplicitDFTModelBuilder.h"
 #include "storm-dft/storage/dft/DFTIsomorphism.h"
-#include "storm-dft/api/storm-dft.h"
-
 
 
 // Thin wrapper for DFT analysis
@@ -28,12 +26,17 @@ void define_analysis(py::module& m) {
     ;
 
     m.def("analyze_dft", &analyzeDFT<double>, "Analyze the DFT", py::arg("dft"), py::arg("properties"), py::arg("symred")=true, py::arg("allow_modularisation")=false, py::arg("relevant_events")=storm::utility::RelevantEvents(), py::arg("allow_dc_for_relevant")=false);
+    m.def("analyze_parametric_dft", &analyzeDFT<storm::RationalFunction>, "Analyze the parametric DFT", py::arg("dft"), py::arg("properties"), py::arg("symred")=true, py::arg("allow_modularisation")=false, py::arg("relevant_events")=storm::utility::RelevantEvents(), py::arg("allow_dc_for_relevant")=false);
 
     m.def("transform_dft", &storm::api::applyTransformations<double>, "Apply transformations on DFT", py::arg("dft"), py::arg("unique_constant_be"), py::arg("binary_fdeps"));
+    m.def("transform_parametric_dft", &storm::api::applyTransformations<storm::RationalFunction>, "Apply transformations on parametric DFT", py::arg("dft"), py::arg("unique_constant_be"), py::arg("binary_fdeps"));
 
     m.def("compute_dependency_conflicts", &storm::api::computeDependencyConflicts<double>, "Set conflicts between FDEPs. Is used in analysis.", py::arg("dft"), py::arg("use_smt") = false, py::arg("solver_timeout") = 0);
+    m.def("compute_dependency_conflicts_parametric", &storm::api::computeDependencyConflicts<storm::RationalFunction>, "Set conflicts between FDEPs. Is used in analysis.", py::arg("dft"), py::arg("use_smt") = false, py::arg("solver_timeout") = 0);
 
     m.def("is_well_formed", &storm::api::isWellFormed<double>, "Check whether DFT is well-formed.", py::arg("dft"), py::arg("check_valid_for_analysis") = true);
+    m.def("is_well_formed_parametric", &storm::api::isWellFormed<storm::RationalFunction>, "Check whether parametric DFT is well-formed.", py::arg("dft"), py::arg("check_valid_for_analysis") = true);
 
     m.def("compute_relevant_events", &storm::api::computeRelevantEvents<double>, "Compute relevant event ids from properties and additional relevant names", py::arg("dft"), py::arg("properties"), py::arg("additional_relevant_names") = std::vector<std::string>());
+    m.def("compute_relevant_events_parametric", &storm::api::computeRelevantEvents<storm::RationalFunction>, "Compute relevant event ids from properties and additional relevant names", py::arg("dft"), py::arg("properties"), py::arg("additional_relevant_names") = std::vector<std::string>());
 }
--- src/dft/io.cpp
+++ src/dft/io.cpp
@@ -7,9 +7,12 @@ void define_input(py::module& m) {
 
     // Load DFT input
     m.def("load_dft_galileo_file", &storm::api::loadDFTGalileoFile<double>, "Load DFT from Galileo file", py::arg("path"));
+    m.def("load_parametric_dft_galileo_file", &storm::api::loadDFTGalileoFile<storm::RationalFunction>, "Load parametric DFT from Galileo file", py::arg("path"));
     // Parse Jani model
     m.def("load_dft_json_file", &storm::api::loadDFTJsonFile<double>, "Load DFT from JSON file", py::arg("path"));
     m.def("load_dft_json_string", &storm::api::loadDFTJsonString<double>, "Load DFT from JSON string", py::arg("json_string"));
+    m.def("load_parametric_dft_json_file", &storm::api::loadDFTJsonFile<storm::RationalFunction>, "Load parametric DFT from JSON file", py::arg("path"));
+    m.def("load_parametric_dft_json_string", &storm::api::loadDFTJsonString<storm::RationalFunction>, "Load parametric DFT from JSON string", py::arg("json_string"));
 }
 
 void define_output(py::module& m) {
@@ -17,4 +20,6 @@ void define_output(py::module& m) {
     // Export DFT
     m.def("export_dft_json_file", &storm::api::exportDFTToJsonFile<double>, "Export DFT to JSON file", py::arg("dft"), py::arg("path"));
     m.def("export_dft_json_string", &storm::api::exportDFTToJsonString<double>, "Export DFT to JSON string", py::arg("dft"));
+    m.def("export_parametric_dft_json_file", &storm::api::exportDFTToJsonFile<storm::RationalFunction>, "Export parametric DFT to JSON file", py::arg("dft"), py::arg("path"));
+    m.def("export_parametric_dft_json_string", &storm::api::exportDFTToJsonString<storm::RationalFunction>, "Export parametric DFT to JSON string", py::arg("dft"));
 }
