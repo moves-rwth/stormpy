17fcae1cdd4ee9dc21e2f25816040d77914d083f
Sebastian Junges <sebastian.junges@ru.nl>
2022-07-28 10:50:50 +0200

    test whether matplotlib is available, added test if matplotlib is not available

--- tests/configurations.py
+++ tests/configurations.py
@@ -8,16 +8,21 @@ has_dft = config.storm_with_dft
 has_gspn = config.storm_with_gspn
 has_pars = config.storm_with_pars
 
-# Check if numpy is available
 try:
     import numpy
-
     has_numpy = True
 except ImportError:
     has_numpy = False
+
+try:
+    import matplotlib
+    has_matplotlib = True
+except ImportError:
+    has_matplotlib = False
 
 xml = pytest.mark.skipif(not has_xml, reason="No support for XML via xerces")
 dft = pytest.mark.skipif(not has_dft, reason="No support for DFTs")
 gspn = pytest.mark.skipif(not has_gspn, reason="No support for GSPNs")
 pars = pytest.mark.skipif(not has_pars, reason="No support for parametric model checking")
 numpy_avail = pytest.mark.skipif(not has_numpy, reason="Numpy not available")
+plotting = pytest.mark.skipif(not has_matplotlib, reason="Matplotlib not available")
--- tests/core/test_multiobjective.py
+++ tests/core/test_multiobjective.py
@@ -2,10 +2,23 @@ import stormpy
 from stormpy.utility.multiobjective_plotting import prepare_multiobjective_result_for_plotting, plot_convex_pareto_curve_demo
 import stormpy.logic
 from helpers.helper import get_example_path
-import matplotlib.pyplot as plt
+
+from configurations import plotting, numpy_avail
 
 class TestModelChecking:
-    def naive_api_double_test(self):
+    def naive_api_double_no_plotting_test(self):
+        program = stormpy.parse_prism_program(get_example_path("mdp", "multiobjective1.nm"))
+        properties = stormpy.parse_properties_for_prism_program("multi(Pmax=? [ F<=3 s=2 ],R{\"rew\"}max=? [ F s=2 ])", program)
+        model = stormpy.build_model(program, properties)
+        result = stormpy.model_checking(model,properties[0])
+        assert len(result.get_underapproximation().vertices) == 3
+        assert len(result.get_overapproximation().vertices) == 6
+
+    @plotting
+    @numpy_avail
+    def naive_api_double_with_plotting_test(self):
+        import matplotlib.pyplot as plt
+
         program = stormpy.parse_prism_program(get_example_path("mdp", "multiobjective1.nm"))
         properties = stormpy.parse_properties_for_prism_program("multi(Pmax=? [ F<=3 s=2 ],R{\"rew\"}max=? [ F s=2 ])", program)
         model = stormpy.build_model(program, properties)
