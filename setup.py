9fa4144887bf42faf2cfdfe01af59afd292e8f5d
Matthias Volk <m.volk@utwente.nl>
2022-07-31 18:39:56 +0200

    Updated required Storm and pycarl versions

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info[0] == 2:
     sys.exit('Sorry, Python 2.x is not supported')
 
 # Minimal storm version required
-storm_min_version = "1.6.5"
+storm_min_version = "1.7.0"
 
 # Get the long description from the README file
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
@@ -274,7 +274,7 @@ setup(
 
     cmdclass={'build_ext': CMakeBuild},
     zip_safe=False,
-    install_requires=['pycarl>=2.0.5'],
+    install_requires=['pycarl>=2.1.0'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'nbval', 'numpy'],
     extras_require={
