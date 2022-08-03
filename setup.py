c5b6af627181449f09c828514fb8dd3df01b624b
Matthias Volk <m.volk@utwente.nl>
2022-08-03 14:33:28 +0200

    Improved build process in CMake and setup.py

--- setup.py
+++ setup.py
@@ -24,10 +24,9 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir='', subdir=''):
+    def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
-        self.subdir = subdir
 
 
 class CMakeBuild(build_ext):
@@ -95,8 +94,10 @@ class CMakeBuild(build_ext):
             sys.exit("Pycarl - Error: carl version {} from '{}' is not supported!".format(carl_version, carl_dir))
 
         # Check additional support
-        use_cln = cmake_conf.CARL_WITH_CLN and not self.config.get_as_bool("disable_cln")
-        use_parser = cmake_conf.CARL_WITH_PARSER and not self.config.get_as_bool("disable_parser")
+        enable_parser = not self.config.get_as_bool("disable_parser")
+        enable_cln = not self.config.get_as_bool("disable_cln")
+        use_parser = enable_parser and cmake_conf.CARL_WITH_PARSER
+        use_cln = enable_cln and cmake_conf.CARL_WITH_CLN
 
         # Print build info
         print("Pycarl - Using carl {} from {}".format(carl_version, carl_dir))
@@ -109,41 +110,39 @@ class CMakeBuild(build_ext):
         else:
             print("Pycarl - Warning: No support for CLN!")
 
-        # Set general cmake build options
         build_type = 'Debug' if self.config.get_as_bool("debug") else 'Release'
-        cmake_args = ['-DPYTHON_EXECUTABLE=' + sys.executable]
+        # Set cmake build options
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + self._extdir("core")]
+        cmake_args += ['-DPYTHON_EXECUTABLE=' + sys.executable]
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + build_type]
         if carl_dir is not None:
             cmake_args += ['-Dcarl_DIR=' + carl_dir]
         if use_parser and carl_parser_dir:
             cmake_args += ['-Dcarl_parser_DIR=' + carl_parser_dir]
+        cmake_args += ['-DUSE_PARSER=' + ('ON' if enable_parser else 'OFF')]
+        cmake_args += ['-DUSE_CLN_NUMBERS=' + ('ON' if enable_cln else 'OFF')]
+
+        # Configure extensions
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''), self.distribution.get_version())
+        setup_helper.ensure_dir_exists(self.build_temp)
+        print("Pycarl - CMake args={}".format(cmake_args))
+        # Call cmake
+        subprocess.check_call(['cmake', os.path.abspath("")] + cmake_args, cwd=self.build_temp, env=env)
+
+        # Set build args
         build_args = ['--config', build_type]
         build_args += ['--', '-j{}'.format(self.config.get_as_int("jobs"))]
-
         # Build extensions
         for ext in self.extensions:
-            setup_helper.ensure_dir_exists(os.path.join(self._extdir(ext.name), ext.subdir))
-            if "core" in ext.name:
-                with open(os.path.join(self._extdir(ext.name), ext.subdir, "_config.py"), "w") as f:
-                    f.write("# Generated from setup.py at {}\n".format(datetime.datetime.now()))
-                    f.write('CARL_VERSION = "{}"\n'.format(carl_version))
-                    f.write("CARL_WITH_PARSER = {}\n".format(use_parser))
-                    f.write("CARL_WITH_CLN = {}\n".format(use_cln))
-            if "cln" in ext.name:
-                with open(os.path.join(self._extdir(ext.name), ext.subdir, "_config.py"), "w") as f:
-                    f.write("# Generated from setup.py at {}\n".format(datetime.datetime.now()))
-                    f.write("CARL_WITH_CLN = {}\n".format(use_cln))
-                if not use_cln:
-                    print("Pycarl - CLN bindings skipped")
-                    continue
-            if "parse" in ext.name:
-                with open(os.path.join(self._extdir(ext.name), ext.subdir, "_config.py"), "w") as f:
-                    f.write("# Generated from setup.py at {}\n".format(datetime.datetime.now()))
-                    f.write("CARL_WITH_PARSER = {}\n".format(use_parser))
-                if not use_parser:
-                    print("Pycarl - Parser bindings skipped")
-                    continue
-            self.build_extension(ext, cmake_args, build_args)
+            if "cln" in ext.name and not use_cln:
+                print("Pycarl - Skipped CLN bindings")
+                continue
+            if "parse" in ext.name and not use_parser:
+                print("Pycarl - Skipped parser bindings")
+                continue
+            # Call make
+            subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
 
     def initialize_options(self):
         build_ext.initialize_options(self)
@@ -166,20 +165,6 @@ class CMakeBuild(build_ext):
         self.config.update("disable_parser", self.disable_parser)
         self.config.update("debug", self.debug)
         self.config.update("jobs", self.jobs)
-
-    def build_extension(self, ext, general_cmake_args, general_build_args):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + os.path.join(extdir, ext.subdir)] + general_cmake_args
-        build_args = general_build_args
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        setup_helper.ensure_dir_exists(self.build_temp)
-        print("Pycarl - CMake args={}".format(cmake_args))
-        # Call cmake
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
 
 
 setup(
@@ -208,16 +193,16 @@ setup(
     package_dir={'': 'lib'},
     include_package_data=True,
     ext_package='pycarl',
-    ext_modules=[CMakeExtension('core', subdir=''),
-                 CMakeExtension('cln', subdir='cln'),
-                 CMakeExtension('gmp', subdir='gmp'),
-                 CMakeExtension('formula', subdir='formula'),
-                 CMakeExtension('formula-cln', subdir='cln/formula'),
-                 CMakeExtension('formula-gmp', subdir='gmp/formula'),
-                 CMakeExtension('parse', subdir='parse'),
-                 CMakeExtension('parse-gmp', subdir='gmp/parse'),
-                 CMakeExtension('parse-cln', subdir='cln/parse')],
-
+    ext_modules=[CMakeExtension('core'),
+                 CMakeExtension('cln'),
+                 CMakeExtension('gmp'),
+                 CMakeExtension('formula'),
+                 CMakeExtension('formula-cln'),
+                 CMakeExtension('formula-gmp'),
+                 CMakeExtension('parse'),
+                 CMakeExtension('parse-gmp'),
+                 CMakeExtension('parse-cln')
+                 ],
     cmdclass={'build_ext': CMakeBuild},
     zip_safe=False,
     setup_requires=['pytest-runner'],
