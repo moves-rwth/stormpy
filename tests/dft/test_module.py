d427dde20b03880efac39873550957400ba46b45
Matthias Volk <m.volk@utwente.nl>
2022-10-02 23:38:57 +0200

    Fix for DFT modules

--- tests/dft/test_module.py
+++ tests/dft/test_module.py
@@ -15,20 +15,20 @@ class TestModule:
         assert dft.nr_dynamic() == 2
         module = dft.modules();
         assert dft.get_element(module.representative()).name == "n0"
-        assert len(module.submodules()) == 3
-        assert len(module.elements()) == 2
+        assert len(module.elements()) == 1
+        assert len(module.submodules()) == 4
         for submodule in module.submodules():
-            assert dft.get_element(submodule.representative()).name in ["n137", "n120", "n21"]
-            assert len(submodule.elements()) in [3, 4, 14]
-            assert len(submodule.submodules()) == 0
+            assert dft.get_element(submodule.representative()).name in ["n116", "n137", "n120", "n21"]
+            assert len(submodule.elements()) in [1, 7]
+            assert len(submodule.submodules()) in [0, 2, 3, 7]
 
     def test_module_json(self):
         dft = stormpy.dft.load_dft_galileo_file(get_example_path("dft", "hecs.dft"))
         json = stormpy.dft.modules_json(dft)
         assert json['representative']['name'] == "n0"
-        assert len(json['elements']) == 2
-        assert len(json['submodules']) == 3
+        assert len(json['elements']) == 1
+        assert len(json['submodules']) == 4
         for submodule in json["submodules"]:
-            assert submodule['representative']['name'] in ["n137", "n120", "n21"]
-            assert len(submodule['elements']) in [3, 4, 14]
-            assert len(submodule['submodules']) == 0
+            assert submodule['representative']['name'] in ["n116", "n137", "n120", "n21"]
+            assert len(submodule['elements']) in [1, 7]
+            assert len(submodule['submodules']) in [0, 2, 3, 7]
