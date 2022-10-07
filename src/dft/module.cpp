d0a8752393630090d16f5d1336d14f8996e2b971
Matthias Volk <m.volk@utwente.nl>
2022-10-07 17:05:59 +0200

    Added single_be to DFT modules

--- src/dft/module.cpp
+++ src/dft/module.cpp
@@ -9,6 +9,7 @@ void define_module(py::module& m) {
     py::class_<DftIndependentModule, std::shared_ptr<DftIndependentModule>>(m, "DftIndependentModule", "Independent module in DFT")
         .def("static", &DftIndependentModule::isStatic, "Whether the module contains only static elements (except in submodules)")
         .def("fully_static", &DftIndependentModule::isFullyStatic, "Whether the module contains only static elements (also in submodules)")
+        .def("single_be", &DftIndependentModule::isSingleBE, "Whether the module consists of a single BE (trivial module)")
         .def("representative", &DftIndependentModule::getRepresentative, "Get module representative")
         .def("elements", &DftIndependentModule::getElements, "Get elements of module (excluding submodules)")
         .def("submodules", &DftIndependentModule::getSubModules, "Get submodules")
