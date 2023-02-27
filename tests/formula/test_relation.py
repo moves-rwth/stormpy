3061f97a310256153bf1ca9c2ffe374b4dcf6798
Matthias Volk <m.volk@utwente.nl>
2023-02-27 11:44:17 +0100

    Added py::prepend to fix overlead order of __str__ for enums

---
+++ tests/formula/test_relation.py
@@ -0,0 +1,9 @@
+import pycarl
+from pycarl.formula import Relation
+
+
+class TestRelation():
+    def test_to_string(self):
+        r = Relation.LESS
+        assert str(r) == "<"
+        assert str(Relation.GEQ) == ">="
