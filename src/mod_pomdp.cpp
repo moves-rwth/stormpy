bc3e89b3fc5871c15fc5313e722a26e2fd006313
Alex Bork <alexander.bork@cs.rwth-aachen.de>
2023-06-28 16:18:35 +0200

    Add bindings for quantitative POMDP analysis

--- src/mod_pomdp.cpp
+++ src/mod_pomdp.cpp
@@ -5,6 +5,7 @@
 #include "pomdp/qualitative_analysis.h"
 #include "pomdp/transformations.h"
 #include "pomdp/memory.h"
+#include "pomdp/quantitative_analysis.h"
 #include <storm/adapters/RationalFunctionAdapter.h>
 
 PYBIND11_MODULE(pomdp, m) {
@@ -22,6 +23,7 @@ PYBIND11_MODULE(pomdp, m) {
     define_transformations_nt(m);
     define_transformations<double>(m, "Double");
     define_transformations<storm::RationalNumber>(m, "Exact");
+    define_belief_exploration<double>(m, "Double");
 
     define_transformations<storm::RationalFunction>(m, "Rf");
 }
---
+++ src/pomdp/quantitative_analysis.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "common.h"
+
+template<typename VT>
+void define_belief_exploration(py::module& m, std::string const& vtSuffix);
