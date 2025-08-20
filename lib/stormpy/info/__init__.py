19d00505cec48dbc5f77140cfd8861ec9c357639
Sebastian Junges <sebastian.junges@ru.nl>
2025-08-20 23:18:41 +0200

    info on storm

--- lib/stormpy/info/__init__.py
+++ lib/stormpy/info/__init__.py
@@ -11,6 +11,39 @@ def storm_version() -> str:
     """
     return _config.STORM_VERSION
 
+def storm_from_system() -> bool:
+    """
+    Does StormPy rely on a (pre-existing) system installation of Storm.
+    System versions include anything on the system
+    that was not fetched during the build of stormpy.
+
+    :return: True, if  stormpy linked against a preexisting storm installation.
+    """
+    return _config.STORM_FROM_SYSTEM
+
+def storm_development_version() -> bool:
+    """
+    Is true, if the version of storm was marked as modified since release.
+    :return: True if a development version
+    """
+    return _config.STORM_DEVELOPER_VERSION
+
+def storm_origin_info() -> [str|None,str|None]:
+    """
+    Information about the source of storm, in particular the repo path and the repo tag.
+
+    If storm was already on the system during installation and this preexisting version was used,
+     these values are expected to be None.
+    If storm was installed (locally) as part of the installation process, these values are expected to be not none.
+    Then:
+      - The repo path usually takes the form of a url
+      - The tag refers to a tag on the repo.
+    A noteworthy exception is when the fetch was from a local source dir.
+    In that case, this source dir is included, and the tag reads "__local-source-dir__"
+
+    :return: A pair with the repo path and the repo tag.
+    """
+    return (_config.STORM_ORIGIN_REPO, _config.STORM_ORIGIN_TAG)
 
 def storm_exact_use_cln() -> bool:
     """
@@ -20,7 +53,6 @@ def storm_exact_use_cln() -> bool:
     :rtype: bool
     """
     return _config.STORM_CLN_EA
-
 
 def storm_ratfunc_use_cln() -> bool:
     """
