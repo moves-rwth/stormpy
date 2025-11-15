d2192fbd12feaa25e8b0b3f888ec2b59d369c274
Sebastian Junges <sebastian.junges@ru.nl>
2025-11-15 12:16:50 +0100

    i am an idiot :)

--- tests/storage/test_jani.py
+++ tests/storage/test_jani.py
@@ -10,7 +10,10 @@ class TestJani:
         assert information.nr_automata == 5
         assert information.nr_edges == 31
         assert information.nr_variables == 18
+
+    def test_variables(self):
+        model, properties = stormpy.parse_jani_model(get_example_path("dtmc", "brp.jani"))
         var_s = model.global_variables.get_variable_by_name("s")
         assert var_s.type.is_bounded_type
         assert var_s.type.lower_bound.evaluate_as_int() == 0
-        assert var_s.type.upper_bound.evaluate_as_int() == 5
+        assert var_s.type.upper_bound.evaluate_as_int() == 6
