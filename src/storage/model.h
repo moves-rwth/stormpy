3ed6e2065dba1ca24a8e0625fc209bf58e4e7179
Matthias Volk <m.volk@tue.nl>
2025-11-21 18:03:39 +0100

    Unified declaration of (parametric) models

--- src/storage/model.h
+++ src/storage/model.h
@@ -6,8 +6,6 @@
 void define_model(py::module& m);
 template<typename ValueType>
 void define_sparse_model(py::module& m, std::string const& vtSuffix);
-void define_sparse_parametric_model(py::module& m);
 void define_sparse_interval_model(py::module& m);
-
-template<storm::dd::DdType DdType>
+template<storm::dd::DdType DdType, typename ValueType>
 void define_symbolic_model(py::module& m, std::string vt_suffix);
