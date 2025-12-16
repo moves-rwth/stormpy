cd2c4952273e46f208bc9887d5ecc5214e927060
Alex Bork <alexander.bork@cs.rwth-aachen.de>
2025-12-16 18:38:56 +0100

    Remove check for number of cut-off schedulers

--- tests/pomdp/test_pomdp_quantitative_analysis.py
+++ tests/pomdp/test_pomdp_quantitative_analysis.py
@@ -24,7 +24,6 @@ class TestPomdpQuantitative:
         assert math.isinf(result.upper_bound)
         assert result.induced_mc_from_scheduler.nr_states == 10
         assert result.induced_mc_from_scheduler.nr_transitions == 23
-        assert len(result.cutoff_schedulers) == 4
 
     def test_underapprox_mc_maze_rmin(self):
         program = stormpy.parse_prism_program(get_example_path("pomdp", "maze_2.prism"))
@@ -41,7 +40,6 @@ class TestPomdpQuantitative:
         assert math.isclose(result.upper_bound, 12.27043, abs_tol=10**-4)
         assert result.induced_mc_from_scheduler.nr_states == 9
         assert result.induced_mc_from_scheduler.nr_transitions == 17
-        assert len(result.cutoff_schedulers) == 3
 
     def test_underapprox_mc_cmaze_rmin(self):
         program = stormpy.parse_prism_program(get_example_path("pomdp", "maze-concise.prism"))
@@ -58,4 +56,3 @@ class TestPomdpQuantitative:
         assert math.isclose(result.upper_bound, 19.78127, abs_tol=10**-4)
         assert result.induced_mc_from_scheduler.nr_states == 9
         assert result.induced_mc_from_scheduler.nr_transitions == 15
-        assert len(result.cutoff_schedulers) == 3
