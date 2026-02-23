f0ce3d4e259f28efc1d78e2c5e4a4ff3954d8838
Sebastian Junges <sebastian.junges@ru.nl>
2026-02-23 09:23:29 +0100

    more bughunting

--- tests/storage/test_memorystructure.py
+++ tests/storage/test_memorystructure.py
@@ -31,14 +31,14 @@ class TestMemoryStructure:
     #     product_memorystructure = memorystructure_one.product(memorystructure_two)
     #     assert product_memorystructure.nr_states == 4
 
-    def test_product_model(self):
-        program = stormpy.parse_prism_program(get_example_path("dtmc", "die.pm"))
-        formulas = stormpy.parse_properties_for_prism_program('P=? [ F "one" ]', program)
-        model = stormpy.build_model(program, formulas)
-        goalstates = model.labeling.get_states("one")
-        memorystructure_one = self._build_memory_structure(model, goalstates)
-        product_type = memorystructure_one.product_model(model)
-        product_model = product_type.build()
-        assert product_model.model_type == stormpy.ModelType.DTMC
-        assert product_model.nr_states == 25
+    # def test_product_model(self):
+    #     program = stormpy.parse_prism_program(get_example_path("dtmc", "die.pm"))
+    #     formulas = stormpy.parse_properties_for_prism_program('P=? [ F "one" ]', program)
+    #     model = stormpy.build_model(program, formulas)
+    #     goalstates = model.labeling.get_states("one")
+    #     memorystructure_one = self._build_memory_structure(model, goalstates)
+    #     product_type = memorystructure_one.product_model(model)
+    #     product_model = product_type.build()
+    #     assert product_model.model_type == stormpy.ModelType.DTMC
+    #     assert product_model.nr_states == 25
 
